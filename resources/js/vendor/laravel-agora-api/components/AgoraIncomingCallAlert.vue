eeb30b310358c1a7e6395f85a95b5c3b85868571
Kyle Barney <kyle@kylebarney.dev>
2021-03-27 11:40:38 -0600

    Tipoff base app + auth installed

---
+++ resources/js/vendor/laravel-agora-api/components/AgoraIncomingCallAlert.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="incoming-call-alert" v-if="callIncoming">
+        <div>
+            <p>Incoming call from: {{ incomingCaller }}</p>
+
+            <div class="btn-group" role="group">
+                <button @click="acceptCall">
+                    Accept
+                </button>
+
+                <button @click="declineCall">
+                    Decline
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AgoraIncomingCallAlert",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/vendor/laravel-agora-api/components/AgoraVideoDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="video-container" v-if="connected">
+        <div id="local-video"></div>
+        <div id="remote-video"></div>
+
+        <div class="call-actions">
+            <button type="button" @click="toggleAudio">
+                {{ sendAudio ? "Mute" : "Unmute" }}
+            </button>
+
+            <button type="button" @click="toggleVideo">
+                {{ sendVideo ? "Hide Video" : "Show Video" }}
+            </button>
+
+            <button type="button" @click="endCall">
+                End Call
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AgoraVideoDisplay",
+}
+</script>
+
+<style scoped>
+
+</style>
