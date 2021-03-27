a4c82825fb2ad6148ab7d2515a470090a77e63c1
Kyle Barney <kyle@kylebarney.dev>
2021-03-27 14:17:10 -0600

    Vue compiles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,15 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+// import Vue from 'vue';
+
+window.Vue = require('vue');
+
+Vue.component('agora-video-display', require('./vendor/laravel-agora-api/components/AgoraVideoDisplay.vue').default);
+Vue.component('agora-user-list', require('./vendor/laravel-agora-api/components/AgoraUserList.vue').default);
+Vue.component('agora-incoming-call-alert', require('./vendor/laravel-agora-api/components/AgoraIncomingCallAlert.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
